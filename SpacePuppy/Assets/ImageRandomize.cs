3b28b029f612f72e520754b91c51876f52f60692
MikloCO <37591806+MikloCO@users.noreply.github.com>
2018-04-13 16:19:24 +0200

    added randomscene
    lol

--- SpacePuppy/Assets/ImageRandomize.cs
+++ SpacePuppy/Assets/ImageRandomize.cs
@@ -1,17 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Random.Range;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ImageRandomize : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Image randomImage;
+    public Sprite Piece1;
+    public Sprite piece2;
+    public Sprite piece3;
+    public Sprite piece4;
+    public Sprite[] images;
+
+    private float timer = 0;
+
+    void Start()
+    {
+        images = new Sprite[4];
+        images[0] = Piece1;
+        images[1] = piece2;
+        images[2] = piece3;
+        images[3] = piece4;
+    }
+    private void Update()
+    {
+
+        if (timer > 2)
+        {
+            timer = 0;
+            changeImage();
+        }
+       timer += Time.deltaTime;
+
+    }
+    void changeImage()
+    {
+        int num = UnityEngine.Random.Range(0, images.Length);
+        randomImage.sprite = images[num];
+    }
 }
