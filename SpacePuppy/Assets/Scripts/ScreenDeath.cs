a7f550d3b908841782d35545fd1741a1976bb1e3
Nicaisce <37660246+Nicaisce@users.noreply.github.com>
2018-04-13 14:33:41 +0200

    Level 1 kontroller
    Har fixat kontrollerna för spelarkaraktären samt borders för kameran som stoppar karaktären att lämna kameran

---
+++ SpacePuppy/Assets/Scripts/ScreenDeath.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenDeath : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+	
+
+	void Update () {
+		
+        if(transform.position.y > 3.6)
+        {
+            //rb2d.velocity = Vector3.zero;
+
+            transform.position = new Vector3(transform.position.x, 3.6f, transform.position.z);
+        }
+        else if (transform.position.y < -5)
+        {
+            transform.position = new Vector3(transform.position.x, -5f, transform.position.z);
+        }
+
+    }
+}
