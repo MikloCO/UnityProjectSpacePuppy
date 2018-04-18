8c2d6ab9e92ed50f2da8bc927e62c5d7e3ccf2ba
Cabamacadaf <maccus9@hotmail.com>
2018-04-18 12:35:08 +0200

    Added swipe to project

---
+++ SpacePuppy/Assets/Scripts/DrawLine.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour {
+
+    public LineRenderer lineRenderer;
+    public CircleCollider2D lineCollider;
+    public GameController gameController;
+
+    public bool start = false;
+    public bool drawing = false;
+
+    private bool mousePressed;
+    private Vector3 mousePosition;
+    private List<Vector3> vertexList = new List<Vector3>();
+
+    private void Start () {
+    }
+
+    void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            mousePressed = true;
+            removeLine();
+        }
+
+        if (mousePressed == true && start == true) {
+            drawing = true;
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0;
+            if (!vertexList.Contains(mousePosition)) {
+                vertexList.Add(mousePosition);
+                lineRenderer.positionCount = vertexList.Count;
+                lineRenderer.SetPosition(vertexList.Count - 1, vertexList[vertexList.Count - 1]);
+                lineCollider.transform.position = vertexList[vertexList.Count - 1];
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            mousePressed = false;
+            drawing = false;
+            start = false;
+        }
+	}
+
+    public void SquiggleCollide (){
+        mousePressed = false;
+        start = false;
+        drawing = false;
+        removeLine();
+    }
+
+    public void Finish () {
+        start = false;
+        drawing = false;
+        removeLine();
+        gameController.FinishLevel();
+    }
+
+    private void removeLine () {
+        lineRenderer.positionCount = 0;
+        vertexList.RemoveRange(0, vertexList.Count);
+    }
+}
