6dd6d1de9793ec377896940b154dc3426523f9fe
Ghostwolf7 <37661407+Ghostwolf7@users.noreply.github.com>
2018-04-19 14:32:48 +0200

    Börjat på BackgroundScript
    För rörande bakground

---
+++ SpacePuppy/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+
+    public float backgroundSize;
+
+    private Transform cameraTransform;
+    private Transform[] layers;
+    private float viewZone = 10;
+    private int leftIndex;
+    private int rightIndex;
+
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        layers = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+            layers[i] = transform.GetChild(i);
+
+        leftIndex = 0;
+        rightIndex = layers.Length - 1;
+
+    }
+
+    private void ScrollLeft() {
+        int lastRight = rightIndex;
+        layers[rightIndex].position = Vector3.right * (layers[leftIndex].position.x - backgroundSize);
+        leftIndex = rightIndex;
+        rightIndex--;
+        if (rightIndex < 0)
+            rightIndex = layers.Length - 1;
+    }
+}
