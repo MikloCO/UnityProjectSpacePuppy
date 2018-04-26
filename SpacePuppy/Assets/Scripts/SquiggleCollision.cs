556412123d6b89bfe7b5cd027e5852d5a6026d95
Cabamacadaf <maccus9@hotmail.com>
2018-04-26 12:15:37 +0200

    restored things

---
+++ SpacePuppy/Assets/Scripts/SquiggleCollision.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquiggleCollision : MonoBehaviour
+{
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Line"))
+        {
+            other.transform.parent.GetComponent<DrawLine>().SquiggleCollide();
+        }
+    }
+}
