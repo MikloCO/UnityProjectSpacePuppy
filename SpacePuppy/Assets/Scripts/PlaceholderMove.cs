3d727ed9efab0135b0f75238ce09baef79e914a0
Cabamacadaf <maccus9@hotmail.com>
2018-04-29 18:44:12 +0200

    La in swipe i level 1

--- SpacePuppy/Assets/Scripts/PlaceholderMove.cs
+++ SpacePuppy/Assets/Scripts/PlaceholderMove.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlaceholderMove : MonoBehaviour {
 	public float speed = 0.1f;
+    public bool paused = false;
 	// Use this for initialization
 	void Start () {
 		Transform transform = GetComponent<Transform> ();
@@ -11,6 +12,8 @@ public class PlaceholderMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (new Vector3(-speed,0,0), transform);
+        if (!paused) {
+            transform.Translate(new Vector3(-speed, 0, 0), transform);
+        }
 	}
 }
