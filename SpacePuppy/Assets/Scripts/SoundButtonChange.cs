ce9e29de76ae48cf842d40acad0d657cb9859477
MartaFrojd <xyellowbearx@hotmail.com>
2018-05-02 15:27:44 +0200

    Lot of Changes
    Sorterat upp i Assets
    Tillbaka knappen fixad

---
+++ SpacePuppy/Assets/Scripts/SoundButtonChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundButtonChange : MonoBehaviour {
+
+    public Button currentButton;
+    public Sprite musicOff;
+    public Sprite musicOn;
+    bool buttonPressed = false;
+
+    Sprite myImageComponent;
+
+
+    void Start () {
+
+        musicOn = GetComponent<Sprite>();
+        musicOff = GetComponent<Sprite>();
+		
+	}
+	
+	
+	void Update () {
+        
+        if(buttonPressed == true)
+        {
+
+        }
+    }
+}
