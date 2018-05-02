f9e80e7fa242e1ba2c6da906cdee71437ab27027
MikloCO <37591806+MikloCO@users.noreply.github.com>
2018-05-02 12:18:43 +0200

    hej
    hej

---
+++ SpacePuppy/Assets/ScoreManagerList.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManagerList : MonoBehaviour {
+
+
+	myText=GameObject.Find("MainText").GetComponent<Text>(); 
+
+	Text text = highScoreArrays;
+
+	public highScoreArray[] highScoreArrays;
+
+	void Update() {
+		
+	}
+
+	void StoreScoreInArray() {
+		text = GetComponent <Text> ();
+	}
+}
