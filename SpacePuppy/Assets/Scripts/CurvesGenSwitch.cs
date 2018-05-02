64e2743160454eb5528d03f35b5941f1c3cd911b
Cabamacadaf <maccus9@hotmail.com>
2018-05-02 13:01:43 +0200

    fixade curvesgen för objekt men nu blir det glapp igen

--- SpacePuppy/Assets/Scripts/CurvesGenSwitch.cs
+++ SpacePuppy/Assets/Scripts/CurvesGenSwitch.cs
@@ -13,7 +13,6 @@ public class CurvesGenSwitch : MonoBehaviour {
 	public float resetPosition = 30f;
 	public float background;
     
-
 	void Start () {
 
 	}
@@ -22,28 +21,22 @@ public class CurvesGenSwitch : MonoBehaviour {
 	void Update () {
         if (!paused) {
             for (int i = 0; i < 2; i++) {
-                asteroids[i].transform.Translate(Vector3.left * speed * Time.deltaTime);
+                int j;
+                if(i == 0) {
+                    j = 1;
+                }
+                else {
+                    j = 0;
+                }
+                Transform thisChild = this.gameObject.transform.GetChild(i);
+                Transform otherChild = this.gameObject.transform.GetChild(j);
+                thisChild.Translate(Vector3.left * speed * Time.deltaTime);
+
+                if(thisChild.position.x < endPosition) {
+                    Destroy(thisChild.gameObject);
+                    Instantiate(asteroids[Random.Range(0, 3)], (otherChild.position + Vector3.right * resetPosition), new Quaternion(), this.transform);
+                }
             }
-            //for (int x = 0; x < 2; x++) {
-            //    if (children[i].position.x < endPosition) {
-            //        // children[i].position = new Vector3(resetPosition, 0, 0);
-            //        Destroy(children[i].GetComponent<GameObject>());
-            //       // children[i].GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
-
-            //        if (leadingSprite == 0)
-            //            leadingSprite = 1;
-            //        else
-            //            leadingSprite = 0;
-            //    }
-
-            //    children[leadingSprite].Translate(Vector3.left * speed * Time.deltaTime);
-
-            //    /if (leadingSprite == 0)
-            //        children[1].position = children[leadingSprite].position + Vector3.right * 30f;
-            //    else
-            //        children[0].position = children[leadingSprite].position + Vector3.right * 30f;
-
-            //    i++;
         }
 	}
 }
