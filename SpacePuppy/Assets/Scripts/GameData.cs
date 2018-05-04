893f87fe0adc31b39990c85a3b716826aa1bdf63
MikloCO <37591806+MikloCO@users.noreply.github.com>
2018-05-04 15:28:02 +0200

    Save and go

---
+++ SpacePuppy/Assets/Scripts/GameData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour {
+	private float[] allHighScore;
+	public float getCurrentFloatData() {
+		return allHighScore;
+	}
+}
