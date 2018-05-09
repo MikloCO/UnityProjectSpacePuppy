15888079c1b93d800ad48b6a4f3dc03d5e4c739a
Ghostwolf7 <37661407+Ghostwolf7@users.noreply.github.com>
2018-05-09 12:45:14 +0200

    bajs!
    whaaa

---
+++ SpacePuppy/Assets/SpawnDCPath.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDCPath : MonoBehaviour {
+
+    public GameObject[] dcPaths;
+    public float chanceToSpawn = 50F;
+
+    void Start ()
+    {
+        float random = Random.Range(0F, 100F);
+        if (random < chanceToSpawn)
+        {
+            Spawn();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Spawn()
+    {
+        Instantiate(dcPaths[Random.Range(0, dcPaths.Length)], transform);
+        
+    }
+}
