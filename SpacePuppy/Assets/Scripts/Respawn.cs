75463599b54c41c6ebfa85aac35b2a2a5e7cd69a
Cabamacadaf <maccus9@hotmail.com>
2018-05-11 11:44:11 +0200

    flyttade respawnscriptet till scripts

---
+++ SpacePuppy/Assets/Scripts/Respawn.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D col) {
+        Debug.Log("respawn");
+        transform.parent.GetComponent<HurtPlayer>().respawnPosition = col.gameObject.transform.position.y;
+	}
+}
