e5c481f3aee44380ede187e44fca76bedd0f25d9
MikloCO <37591806+MikloCO@users.noreply.github.com>
2018-05-16 12:42:33 +0200

    CD spawn asteroids

---
+++ SpacePuppy/Assets/Scripts/PickUpCD.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpCD : MonoBehaviour {
+	public AudioClip[] clips;
+	void Start () {	
+	}
+
+	void Update () {
+
+	}
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.CompareTag("CD")) 
+		{
+			AudioClip clip;
+			do {
+				clip = clips [Random.Range (0, clips.Length)];
+			} while(clip.Equals(Camera.main.GetComponent<AudioSource> ().clip));
+			Camera.main.GetComponent<AudioSource> ().clip = clip;
+			Camera.main.GetComponent<AudioSource> ().Play ();
+			Destroy (other.gameObject);
+		}
+	}
+}
