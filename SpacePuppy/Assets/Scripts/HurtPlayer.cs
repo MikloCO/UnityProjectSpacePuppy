dd21e488f2f4c8d12e0a32e9204dc264550c0cef
Cabamacadaf <maccus9@hotmail.com>
2018-05-17 11:35:30 +0200

    particles

--- SpacePuppy/Assets/Scripts/HurtPlayer.cs
+++ SpacePuppy/Assets/Scripts/HurtPlayer.cs
@@ -13,6 +13,8 @@ public class HurtPlayer : MonoBehaviour {
     //public image damageImage;
     private Animator hurtAnim;
 
+    public Transform particleSystem;
+
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f); //oklart om detta behövs
 
     public AudioSource playerAudio;
@@ -31,12 +33,17 @@ public class HurtPlayer : MonoBehaviour {
         if (damaged) {
             damageTimer += Time.deltaTime;
             hurtAnim.SetInteger("state", 3);
+            particleSystem.localPosition = new Vector3(-2.2f, 0.8f);
+            particleSystem.localRotation.Set(particleSystem.localRotation.x, particleSystem.localRotation.y, 90f, particleSystem.localRotation.w);
 
             //     damageImage.color = flashColour;
         }
         if (damageTimer > 0.5f) {
             damaged = false;
             damageTimer = 0;
+            hurtAnim.SetInteger("state", 0);
+            particleSystem.localPosition = new Vector3(-0.51f, -0.6f);
+            particleSystem.localRotation.Set(particleSystem.localRotation.x, particleSystem.localRotation.y, 157.245f, particleSystem.localRotation.w);
         }
         //else
         //{
