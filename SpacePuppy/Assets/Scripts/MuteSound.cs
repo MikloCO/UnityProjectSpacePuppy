a6a59c2618990a5c1d078b68060dd043d6a128c4
MartaFrojd <xyellowbearx@hotmail.com>
2018-05-18 16:04:20 +0200

    Går att pausa men det vill inte sparas till nästa scene

--- SpacePuppy/Assets/Scripts/MuteSound.cs
+++ SpacePuppy/Assets/Scripts/MuteSound.cs
@@ -3,35 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MuteSound : MonoBehaviour {
+public class MuteSound : MonoBehaviour
+{
     private AudioSource menuAudio;
-    public Button MusicON;
+    //public Button MusicON;
 
     public AudioClip backMusic;
     public AudioClip click;
-         
-    void Start ()
-    {
-       menuAudio = GetComponent<AudioSource>();
-       menuAudio.PlayOneShot(backMusic, 1f); // bara när jag startar
-       MusicON = GetComponent<Button>();
 
+    void Start()
+    {
+        menuAudio = GetComponent<AudioSource>();
+        menuAudio.PlayOneShot(backMusic, 1f); // bara när jag startar
+        
     }
 
-    void Update ()
-    {
-         //Audio.pause = true;
-        //AudioListener.volume = 0;
-
-    }
-
-    public void UnMute()
-    {
-     if (MusicON== true)
-        {
-            menuAudio.PlayOneShot(backMusic, 1f);
-
-        }
-
-    }
 }
